9ebf75ddd44a0a16deeb57220b07861561fec141
Dustin Moore <dustin@server.dustinmoore.net>
2012-02-23 22:07:47 -0600

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var UI = {};
+UI.SlideShow = (function() {
+  $(_init);
+
+  var _total_width = 0;
+
+  function _init() {
+  
+    _total_width = $('#content').width();
+
+    if ($('section.panel').length > 1) {
+      $('section.panel').width(_total_width - 2).hide().first().show();
+      $('section.panel p').width(_total_width - 2);
+  
+      $('section.panel').first().before('<div id="panel_prev" class="panel-control disabled"></div>');
+      $('section.panel').last().after('<div id="panel_next" class="panel-control"></div>');
+  
+      $('#panel_prev').click(_prev);
+      $('#panel_next').click(_next);
+  
+      $('#content').height(500);
+    }
+  }
+  
+  function _next() {
+    if ($('section.panel:visible').is('section.panel:last'))
+      return;
+  
+    $('section.panel:visible').animate({width: 'toggle'}).next().animate({width: 'toggle'}, function() {
+  
+      if ($('section.panel:visible').is('section.panel:last')) {
+        $('#panel_next').addClass('disabled');
+  
+      } else {
+        $('#panel_next, #panel_prev').removeClass('disabled');
+      }
+    });
+  }
+  
+  function _prev() {
+    if ($('section.panel:visible').is('section.panel:first'))
+      return;
+  
+    $('section.panel:visible').animate({width: 'toggle'}).prev().animate({width: 'toggle'}, function() {
+  
+      if ($('section.panel:visible').is('section.panel:first')) {
+        $('#panel_prev').addClass('disabled');
+  
+      } else {
+        $('#panel_next, #panel_prev').removeClass('disabled');
+      }
+    });
+  }
+})();
